ca7471413d1e7c9694efcba7be95e6c23add8590
samiaaaah <samiyamahveen16@gmail.com>
2025-06-20 16:34:16 +0530

    feat: Implement server with contact form handling and email notification

---
+++ backend/server.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./config/db');
+
+const Contact = require('./model/user'); 
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const app = express();
+const port = 3012 ;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use(cors({
+  origin: ['https://bimqubator.com', 'https://bimqubator.com','https://wwww.bimqubator.com'],
+  methods: ['GET', 'POST'],
+}));
+app.get('/', (req, res) => {
+  res.send('Hello from Bimqubator!');
+});
+
+app.post('/contact', async (req, res) => {
+  try {
+    const { name, telephone, email, subject, message } = req.body;
+
+    if (!name || !email ||  !message) {
+      return res.status(400).json({ message: 'Required fields missing' });
+    }
+
+    // Save to DB
+    const savedContact = await Contact.create({ name, telephone, email, subject, message });
+
+    const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: parseInt(process.env.SMTP_PORT), 
+  secure: false, 
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  },
+  tls: {
+    rejectUnauthorized: false // allow self-signed certs for now
+  }
+});
+    const mailOptions = {
+      from: `"Bimqubator Contact Form" <${process.env.EMAIL_USER}>`,
+      to: 'info@bimqubator.com',
+      subject: `New Contact from ${name}`,
+      html: `
+        <h3>New Contact Form Submission</h3>
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Telephone:</strong> ${telephone}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Subject:</strong> ${subject || 'No Subject'}</p>
+        <p><strong>Message:</strong><br>${message}</p>
+      `,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(201).json({ message: 'Message submitted & email sent!', data: savedContact });
+  } catch (err) {
+    console.error('Error:', err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+const startServer = async () => {
+  try {
+    await db.authenticate();
+    console.log('✅ Connected to MySQL');
+    await db.sync();
+    console.log('✅ Models synchronized');
+    app.listen(port, () => {
+      console.log(`🚀 Server running at http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error('DB Connection Failed:', err);
+  }
+};
+
+startServer();
